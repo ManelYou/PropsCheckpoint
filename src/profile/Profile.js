ccd304bc746fc10906984deb25021b3d90bfc551
Manel Younsi <manel.younsi@gmail.com>
2022-03-23 20:30:50 +0100

    upload code

---
+++ src/profile/Profile.js
@@ -0,0 +1,30 @@
+import React  from 'react';
+import "./Profile.css";
+import PropTypes from "prop-types";
+
+const Profile = (props) =>{
+
+    return(<div>
+    <h1>FullName: {props.fullName}</h1>
+    <h1>Bio: {props.bio}</h1>
+    <h1>Profession: {props.profession}</h1>
+    <h1>{props.children}</h1> 
+    <button value={props.fullName} onClick={props.onChildClick}>{props.data}</button>
+</div>)
+}
+
+export default Profile;
+
+//Set default props
+Profile.defaultProps = {
+    fullName: "test1",
+    bio:"hh",
+    profession: "dev"
+};
+
+Profile.propTypes = {
+    fullName: PropTypes.string,
+    bio: PropTypes.string,
+    profession: PropTypes.string
+}
+    
